2c333950da7371f93c9d68ad84ed11bee763ff36
Ahmed koraish <136814559+Ahmedkoraish@users.noreply.github.com>
2025-08-04 10:39:53 +0300

    fix(profile): resolve bug in profile page layout and logic

--- Client/src/pages/Profile.jsx
+++ Client/src/pages/Profile.jsx
@@ -3,28 +3,45 @@ import { useUser } from "../features/auth/useUser";
 import EditProfileModal from "../component/EditProfileModal";
 import toast from "react-hot-toast";
 import ChangePasswordModal from "../component/ChangePasswordModal";
+import GuestBooking from "../component/GuestBooking";
+import HostApartment from "../component/HostApartment";
 
 export default function Profile() {
   const { user, refetch } = useUser();
   const [showEditModal, setShowEditModal] = useState(false);
   const [isRefetching, setIsRefetching] = useState(false);
-  const [editPassword,setEditPassword] =useState(false)
+  const [editPassword, setEditPassword] = useState(false);
+  const [activeTab, setActiveTab] = useState("about");
   const curUser = user?.user;
 
   const handleEditSuccess = async () => {
     setIsRefetching(true);
     try {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
       await refetch();
       setShowEditModal(false);
+      toast.success("Profile updated successfully");
     } catch (err) {
       console.error("Failed to refetch user data:", err);
       toast.error(
-        "Profile updated but failed to refresh. Please reload the page."
+        "profile updated but failed to refresh please reload the page."
       );
       setShowEditModal(false);
     } finally {
       setIsRefetching(false);
     }
+  };
+
+  const handlePasswordChangeSuccess = async () => {
+    setEditPassword(false);
+    
+    try {
+      await refetch();
+    } catch (error) {
+      console.error("Failed to refetch user data after password change:", error);
+      toast.error("password changed but failed to refresh user data.");
+    }
+  
   };
 
   return (
@@ -45,11 +62,11 @@ export default function Profile() {
             <p className="text-gray-600">
               Joined in{" "}
               {curUser?.createdAt
-                ? new Date(curUser.createdAt).getFullYear()
+                ? new Date(curUser?.createdAt).getFullYear()
                 : ""}
             </p>
             <div className="badge font-semibold badge-accent">
-              {curUser.role}
+              {curUser?.role}
             </div>
           </div>
         </div>
@@ -64,14 +81,15 @@ export default function Profile() {
             Edit Profile
           </button>
 
-          <button 
-          onClick={()=>setEditPassword(true)}
-          className="sm:w-auto px-11 py-2 border rounded bg-blue-400 text-white hover:bg-blue-700 transition whitespace-nowrap cursor-pointer">
+          <button
+            onClick={() => setEditPassword(true)}
+            className="sm:w-auto px-11 py-2 border rounded bg-blue-400 text-white hover:bg-blue-700 transition whitespace-nowrap cursor-pointer"
+          >
             Change Password
           </button>
         </div>
       </div>
-        {/* editModal */}
+      {/* editModal */}
       {showEditModal && (
         <EditProfileModal
           onClose={() => setShowEditModal(false)}
@@ -83,61 +101,98 @@ export default function Profile() {
       {editPassword && (
         <ChangePasswordModal
           onClose={() => setEditPassword(false)}
+          onSuccess={handlePasswordChangeSuccess}
         />
       )}
-      
 
       {/* tabs */}
       <div className="mt-6 flex flex-wrap border-b gap-2">
-        <button className="px-4 py-2 font-semibold hover:border-b-2 hover:font-bold cursor-pointer">
+        <button
+          className={` cursor-pointer px-4 py-2 font-semibold ${
+            activeTab === "about"
+              ? "text-blue-700"
+              : "text-gray-600 hover:text-black"
+          }`}
+          onClick={() => setActiveTab("about")}
+        >
           About
         </button>
-        <button className="px-4 py-2 font-semibold hover:border-b-2 hover:font-bold cursor-pointer">
+        <button
+          className={` cursor-pointer px-4 py-2 font-semibold ${
+            activeTab === "booking"
+              ? "text-blue-700"
+              : "text-gray-600 hover:text-black"
+          }`}
+          onClick={() => setActiveTab("booking")}
+        >
           {curUser?.role === "host" ? "Your Apartment" : "Your Booking"}
         </button>
       </div>
 
-      {/* confirmed info */}
-      <div className="mt-6">
-        <h3 className="text-lg font-semibold mb-2">Confirmed Information</h3>
-        <div className="p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-1 gap-4">
-          <div className="min-w-0">
-            <p className="text-lg font-semibold text-gray-500">Email Address</p>
-            <p className="break-words">{curUser?.email}</p>
+      {activeTab === "about" && (
+        <>
+          {/* confirmed info */}
+          <div className="mt-6">
+            <h3 className="text-lg font-semibold mb-2">
+              Confirmed Information
+            </h3>
+            <div className="p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-1 gap-4">
+              <div className="min-w-0">
+                <p className="text-lg font-semibold text-gray-500">
+                  Email Address
+                </p>
+                <p className="break-words">{curUser?.email}</p>
+              </div>
+              <div className="min-w-0">
+                <p className="text-lg font-semibold text-gray-500">
+                  Phone Number
+                </p>
+                <p>{curUser?.phoneNumber || "not available yet"}</p>
+              </div>
+              <div className="min-w-0">
+                <p className="text-lg font-semibold text-gray-500">
+                  Is Verified
+                </p>
+                <p>{curUser?.isVerified ? "Verified" : "Not verified"}</p>
+              </div>
+            </div>
           </div>
-          <div className="min-w-0">
-            <p className="text-lg font-semibold text-gray-500">Phone Number</p>
-            <p>{curUser?.phoneNumber || "not available yet"}</p>
-          </div>
-          <div className="min-w-0">
-            <p className="text-lg font-semibold text-gray-500">Is Verified</p>
-            <p>{curUser?.isVerified ? "Verified" : "Not verified"}</p>
-          </div>
-        </div>
-      </div>
 
-      {/* about section */}
-      <div className="mt-6">
-        <h3 className="text-lg font-semibold mb-2">About</h3>
-        <div className="p-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 gap-4">
-          <div className="min-w-0">
-            <p className="text-lg font-semibold text-gray-500">Gender</p>
-            <p>{curUser?.gender || "Not selected yet"}</p>
+          {/* about section */}
+          <div className="mt-6">
+            <h3 className="text-lg font-semibold mb-2">About</h3>
+            <div className="p-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 gap-4">
+              <div className="min-w-0">
+                <p className="text-lg font-semibold text-gray-500">Gender</p>
+                <p>{curUser?.gender || "Not selected yet"}</p>
+              </div>
+              <div className="min-w-0">
+                <p className="text-lg font-semibold text-gray-500">
+                  Birth Date
+                </p>
+                <p className="text-gray-800 mt-1">
+                  {curUser?.dateOfBirth
+                    ? new Date(curUser.dateOfBirth).toLocaleDateString(
+                        "en-GB",
+                        {
+                          day: "2-digit",
+                          month: "2-digit",
+                          year: "numeric",
+                        }
+                      )
+                    : "Not selected yet"}
+                </p>
+              </div>
+            </div>
           </div>
-          <div className="min-w-0">
-            <p className="text-lg font-semibold text-gray-500">Birth Date</p>
-            <p className="text-gray-800 mt-1">
-              {curUser?.dateOfBirth
-                ? new Date(curUser.dateOfBirth).toLocaleDateString("en-GB", {
-                    day: "2-digit",
-                    month: "2-digit",
-                    year: "numeric",
-                  })
-                : "Not selected yet"}
-            </p>
-          </div>
+        </>
+      )}
+
+      {activeTab === "booking" && (
+        <div className="mt-4">
+          {curUser?.role === "guest" ? <GuestBooking /> : <HostApartment />}
         </div>
-      </div>
+      )}
     </div>
   );
 }
